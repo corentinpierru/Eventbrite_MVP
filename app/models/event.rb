88561a1ca350d504f14b00b4397590248a059885
corentinpierru <corentin.pierru@laposte.net>
2020-05-11 21:35:45 +0200

    first commit, init db

---
+++ app/models/event.rb
@@ -0,0 +1,39 @@
+class Event < ApplicationRecord
+  validates :start_date, :presence => { :message => "must be a valid date/time" }
+
+  validates :title, :presence => { :message => "must have a title"}
+  validates :title,
+  length: { in: 6..20 }
+
+  validates :description,
+  length: { in: 20..2000},
+  presence: true
+
+  validates :price, :numericality => {greater_than: 0, less_than:2001}, presence: true
+  
+  validates :location, presence: true 
+
+  private
+  validate :start_date_after_now
+  def start_date_after_now
+    if duration
+      errors.add(:start_date, "must be after now") unless
+      start_date > Time.now
+    end
+  end
+
+  validate :duration_positive
+  def duration_positive
+    if duration
+      errors.add(:duration, "duration must be positive") unless
+      duration > 0
+    end
+  end
+
+  validate :duration_multiple_of_5
+  def duration_multiple_of_5
+    errors.add(:duration, "must be a multiple of 5") unless
+    duration % 5 == 0
+  end
+  
+end
---
+++ db/migrate/20200511172539_create_participations.rb
@@ -0,0 +1,11 @@
+class CreateParticipations < ActiveRecord::Migration[6.0]
+  def change
+    create_table :participations do |t|
+      t.string :strip_customer_id
+      t.belongs_to :user
+      t.belongs_to :event
+
+      t.timestamps
+    end
+  end
+end
