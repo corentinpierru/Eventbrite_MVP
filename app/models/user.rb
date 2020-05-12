cdb4e7b3c1d3b04c6a657be4b29914efe28757e3
corentinpierru <corentin.pierru@laposte.net>
2020-05-12 23:19:32 +0200

    petite modif mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ApplicationRecord
   has_many :administrators, foreign_key: 'administrator_id', class_name: "Event"
   has_many :attendees, foreign_key: 'attendee_id', class_name: "Participation"
 
-  after_create :welcome_send
 
   def welcome_send
     UserMailer.welcome_email(self).deliver_now
